87e5f7a680a191cd7a25e205a3d155c6521137c7
ir001 <irwanantonio2708@gmail.com>
2020-10-22 11:54:06 +0700

    Integrasi Laravel 7 & AdminLTE

---
+++ README.md
@@ -0,0 +1,18 @@
+# Toko Online WhatsApp
+
+Toko Online WhatsApp adalah platform jual beli barang atau jasa secara online yang memanfatkan fitur WhatsApp. Pengguna dapat melakukan pembelian barang atau jasa tanpa harus membuat akun terlebih dahulu. (Masih dalam pengembangan)
+
+## Latar Belakang
+Lorem ipsum
+
+## Usage
+
+```git
+git clone https://github.com/ir001/laravel-toko-online
+```
+
+## Fitur
+Lorem ipsum
+
+## Kontak Saya
+[Irwan Antonio](https://www.facebook.com/ir001.id)
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Auth::routes();
+Route::group(['prefix' => 'admin-panel'], function () {
+    /* Login */
+    Route::get('login', 'Auth\LoginController@showLoginForm');
+    Route::post('login', 'Auth\LoginController@login');
+    /* Logout */
+    Route::post('logout', 'Auth\LoginController@logout');
+    Route::get('logout', 'Auth\LoginController@logout');
+    /* Forgot Password  */
+    Route::match(['get', 'head'], 'password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm');
+    Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::match(['get', 'head'], 'password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/reset', 'Auth\ForgotPasswordController@reset')->name('password.update');
+    Route::match(['get', 'head'], 'password/reset/{token}', 'Auth\ForgotPasswordController@showResetForm')->name('password.reset');
+});
+
+Route::get('admin-panel', 'HomeController@index')->name('home');
